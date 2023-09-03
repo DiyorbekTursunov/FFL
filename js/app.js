6b297ce4a01e29eec21a94a878657f31f93e45fd
Diyorbek <tursunovdiyorbek570@gmail.com>
2023-09-03 18:05:28 +0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+let btnfl1 = document.querySelector('.btnfl1'),
+    close = document.querySelector('.close')
+    inputiconbtn = document.querySelector('.inputiconbtn')
+
+inputiconbtn.addEventListener('click',function (){
+    inputiconbtn.style.display= 'block'
+      })
+      
+      console.log(inputiconbtn);
